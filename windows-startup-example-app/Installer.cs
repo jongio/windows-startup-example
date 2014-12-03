98d3a48972346dcaece4c24d833c2cf7de8ee151
jongallant <jonbgallant@gmail.com>
2014-12-02 23:12:22 -0800

    Adding the example code

---
+++ windows-startup-example-app/Installer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace windows_startup_example_app
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public override void Commit(System.Collections.IDictionary savedState)
+        {
+            base.Commit(savedState);
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.SetValue("windows-startup-example-app", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\windows-startup-example-app.exe");
+            }
+        }
+
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.DeleteValue("windows-startup-example-app", false);
+            }
+        }
+    }
+}
